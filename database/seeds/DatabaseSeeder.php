535624bb8a34fd287817e5e672113f85a1650c2b
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-04-14 14:19:28 +0700

    init template

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Admin Kyoo',
+            'email' => 'admin@kyoo.id',
+            'password' => bcrypt('secret123'),
+        ]);
     }
 }
