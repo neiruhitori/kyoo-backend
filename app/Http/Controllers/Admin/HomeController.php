260561d83833121241b044b57d8c8a9dc45570f6
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-04-18 16:52:14 +0700

    update dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Branch;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        $branches = Branch::all();
+        return view('admin.home', [
+            'totalBranch' => count($branches)
+        ]);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,7 +11,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Branches</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">75</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{number_format($totalBranch)}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
