cfb11498ac755a85ac9424a2f73e9e9a8cc6efbc
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-04-21 21:25:05 +0700

    slot migration

---
+++ app/Slot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Slot extends Model
+{
+    use SoftDeletes;
+    
+    protected $fillable = ['service_id', 'max_slots', 'day', 'start_time', 'end_time'];
+}
---
+++ database/migrations/2020_04_21_142217_create_slots_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id');
+            $table->integer('max_slots')->default(1);
+            $table->enum('day', ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+            $table->softdeletes();
+
+            $table->foreign('service_id')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slots');
+    }
+}
