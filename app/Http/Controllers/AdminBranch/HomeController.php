e7a30e24164a7708304c6573858c3eee28421fb0
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-04-30 14:42:06 +0700

    integrate admin branch graph

--- app/Http/Controllers/AdminBranch/HomeController.php
+++ app/Http/Controllers/AdminBranch/HomeController.php
@@ -8,15 +8,19 @@ use App\Http\Requests\AdminBranch\UpdateProfile;
 use App\User;
 use App\Appointment;
 use Auth;
+use DB;
+
 class HomeController extends Controller
 {
     public function index()
     {
         $appointments = Appointment::whereMonth('date', date('m'))->get();
+        $appointmentGraph = Appointment::select(DB::raw('DAY(date) as `day`'), DB::raw('count(id) as `total`'))->whereMonth('date', date('m'))->groupBy('day')->get();
         return view('adminBranch.home', [
             'totalAppointment' => count($appointments),
             'totalServed' => count($appointments->where('status', 'served')),
             'totalNoShow' => count($appointments->where('status', 'no show')),
+            'appointmentGraph' => $appointmentGraph
         ]);
     }
 
