0c9b480d63194f23613443341b41199224304990
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-05-26 14:02:33 +0700

    change regency_id to int

--- app/Http/Controllers/API/BranchController.php
+++ app/Http/Controllers/API/BranchController.php
@@ -41,6 +41,7 @@ class BranchController extends Controller
 
     public function show(Branch $branch)
     {
+        $branch->regency_id = (int) $branch->regency_id;
         $branch->Schedule;
         $branch->IndustryCategory;
 
