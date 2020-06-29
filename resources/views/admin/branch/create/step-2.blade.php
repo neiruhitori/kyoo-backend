58d50db195de3576cdb80d464d9ccee771bd6b47
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-06-29 22:03:09 +0700

    change maps on create branch

--- resources/views/admin/branch/create/step-2.blade.php
+++ resources/views/admin/branch/create/step-2.blade.php
@@ -113,24 +113,52 @@
 		function initMaps() {
             $('#map-text').hide()
             /**
-            * An event listener is added to listen to tap events on the map.
-            * Clicking on the map displays an alert box containing the latitude and longitude
-            * of the location pressed.
-            * @param  {H.Map} map      A HERE Map instance within the application
+            * Calculates and displays the address details of  425 Randolph St, Chicago, IL
+            * based on a structured input
+            *
+            *
+            * A full list of available request parameters can be found in the Geocoder API documentation.
+            * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-geocode.html
+            *
+            * @param   {H.service.Platform} platform    A stub class to access HERE services
             */
-            function setUpClickListener(map) {
-            // Attach an event listener to map display
-            // obtain the coordinates and display in an alert box.
-            map.addEventListener('tap', function (evt) {
-                var coord = map.screenToGeo(evt.currentPointer.viewportX,
-                        evt.currentPointer.viewportY);
-                logEvent('Clicked at ' + Math.abs(coord.lat.toFixed(4)) +
-                    ((coord.lat > 0) ? 'N' : 'S') +
-                    ' ' + Math.abs(coord.lng.toFixed(4)) +
-                    ((coord.lng > 0) ? 'E' : 'W'));
-                    $('#latInput').val(coord.lat)
-                    $('#lngInput').val(coord.lng)
-            });
+            function geocode(platform) {
+            let city = $('#regency_id option:selected').text()
+            var geocoder = platform.getGeocodingService(),
+                geocodingParameters = {
+                    city,
+                    country: 'indonesia',
+                    jsonattributes : 1
+                };
+
+            geocoder.geocode(
+                geocodingParameters,
+                onSuccess,
+                onError
+            );
+            }
+            /**
+            * This function will be called once the Geocoder REST API provides a response
+            * @param  {Object} result          A JSONP object representing the  location(s) found.
+            *
+            * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-type-response-geocode.html
+            */
+            function onSuccess(result) {
+            var locations = result.response.view[0].result;
+            /*
+            * The styling of the geocoding response on the map is entirely under the developer's control.
+            * A representitive styling can be found the full JS + HTML code of this example
+            * in the functions below:
+            */
+            addLocationsToMap(locations);
+            }
+
+            /**
+            * This function will be called if a communication error occurs during the JSON-P request
+            * @param  {Object} error  The error message received.
+            */
+            function onError(error) {
+            alert('Can\'t reach the remote server');
             }
 
             /**
@@ -144,41 +172,85 @@
             });
             var defaultLayers = platform.createDefaultLayers();
 
-            //Step 2: initialize a map
+            //Step 2: initialize a map - this map is centered over Jakarta
             var map = new H.Map(document.getElementById('map'),
             defaultLayers.vector.normal.map,{
-                center: {lat: -6.175392, lng: 106.827153},
-                zoom: 13,
-                pixelRatio: window.devicePixelRatio || 1
+            center: {lat: -6.175392, lng: 106.827153},
+            zoom: 17,
+            pixelRatio: window.devicePixelRatio || 1
             });
-
             // add a resize listener to make sure that the map occupies the whole container
             window.addEventListener('resize', () => map.getViewPort().resize());
+
+            var locationsContainer = document.getElementById('panel');
 
             //Step 3: make the map interactive
             // MapEvents enables the event system
             // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
             var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-            // map.getControl('zoom').setEnabled(false)
 
-            // Step 4: create custom logging facilities
-            var logContainer = document.createElement('ul');
-            logContainer.className ='log';
-            logContainer.innerHTML = '<li class="log-entry">Try clicking on the map</li>';
-            map.getElement().appendChild(logContainer);
+            // Create the default UI components
+            var ui = H.ui.UI.createDefault(map, defaultLayers);
 
-            // Helper for logging events
-            function logEvent(str) {
-            var entry = document.createElement('li');
-            entry.className = 'log-entry';
-            entry.textContent = str;
-            logContainer.insertBefore(entry, logContainer.firstChild);
+            // Hold a reference to any infobubble opened
+            var bubble;
+
+            /**
+            * Opens/Closes a infobubble
+            * @param  {H.geo.Point} position     The location on the map.
+            * @param  {String} text              The contents of the infobubble.
+            */
+            function openBubble(position, text){
+            if(!bubble){
+                bubble =  new H.ui.InfoBubble(
+                position,
+                {content: text});
+                ui.addBubble(bubble);
+            } else {
+                bubble.setPosition(position);
+                bubble.setContent(text);
+                bubble.open();
+            }
             }
 
-            setUpClickListener(map);
-            // Create the default UI:
-            var ui = H.ui.UI.createDefault(map, defaultLayers);
-            ui.getControl('zoom').setEnabled(true)
+            /**
+            * Creates a series of H.map.Markers for each location found, and adds it to the map.
+            * @param {Object[]} locations An array of locations as received from the
+            *                             H.service.GeocodingService
+            */
+            function addLocationsToMap(locations){
+            var group = new  H.map.Group(),
+                position,
+                i;
+
+            // Add a marker for each location found
+            for (i = 0;  i < locations.length; i += 1) {
+                position = {
+                lat: locations[i].location.displayPosition.latitude,
+                lng: locations[i].location.displayPosition.longitude
+                };
+                marker = new H.map.Marker(position);
+                marker.label = locations[i].location.address.label;
+                group.addObject(marker);
+            }
+
+            group.addEventListener('tap', function (evt) {
+                map.setCenter(evt.target.getGeometry());
+                openBubble(
+                evt.target.getGeometry(), evt.target.label);
+            }, false);
+
+            // Add the locations group to the map
+            map.addObject(group);
+            map.getViewModel().setLookAtData({
+                bounds: group.getBoundingBox(),
+                zoom: 15
+            });
+            }
+
+            // Now use the map as required...
+            geocode(platform);
+
         }
 	</script>
 @endpush
