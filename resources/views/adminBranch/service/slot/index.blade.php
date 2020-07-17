8783c861465626df034cb484ac85d12b7bbf80a2
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-07-17 08:34:51 +0700

    adding service name on slot list

--- resources/views/adminBranch/service/slot/index.blade.php
+++ resources/views/adminBranch/service/slot/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-12">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">List Slot</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">List Slot: {{ $service->name }}</h6>
                 </div>
                 <div class="card-body">
                     @include('layouts.alert')
