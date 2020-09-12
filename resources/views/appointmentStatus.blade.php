46edebc3d408567f3351682bcb1e6647ddcbc0cc
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-09-12 21:14:07 +0700

    fixing status page

--- resources/views/appointmentStatus.blade.php
+++ resources/views/appointmentStatus.blade.php
@@ -18,11 +18,34 @@
   <!-- Custom styles for this template-->
   <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
   <style>
+    .img-logo {
+        width: 200px;
+        margin-bottom: 20px;
+    }
+    .img-playstore {
+        width: 200px;
+    }
     .bg-gradient-primary {
       background: linear-gradient(121.16deg, #189DCD 0.95%, #0A5194 97.59%);
     }
     .fullwidth {
         width: 100%;
+    }
+    @media(min-width: 300px){
+        p {
+            margin-bottom: 0px;
+        }
+        .col-md-8 {
+            margin-bottom: 16px;
+        }
+        .text-right {
+            text-align: left !important;
+        }
+    }
+    @media(min-width: 700px){
+        .text-right {
+            text-align: right !important;
+        }
     }
   </style>
 </head>
@@ -33,11 +56,11 @@
 
     <!-- Outer Row -->
     <div class="row">
-
-      <div class="col-xl-12 col-lg-12 col-md-9">
+      <div class="col-xl-12 col-lg-12 col-md-12">
         <div class="row">
           <div class="col-md-2"></div>
             <div class="col-md-8">
+                <img src="{{ asset('img/logo.svg') }}" alt="" class="img-logo">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">Appointment Status</h6>
@@ -49,10 +72,36 @@
                             <hr>
                           </div>
                           <div class="col-md-4">
-                              <p>Booking Created</p>
+                              <p>Booking Status</p>
                           </div>
                           <div class="col-md-8 text-right">
-                              <b>{{ $appointment->created_at }}</b>
+                              @switch($appointment->status)
+                                  @case('book')
+                                      <span class="badge badge-primary py-2 px-4" style="font-size: 16px;">
+                                          <b>Book</b>
+                                      </span>
+                                      @break
+                                  @case('check in')
+                                      <span class="badge badge-info py-2 px-4" style="font-size: 16px;">
+                                          <b>Check In</b>
+                                      </span>
+                                      @break
+                                  @case('no show')
+                                      <span class="badge badge-danger py-2 px-4" style="font-size: 16px;">
+                                          <b>No Show</b>
+                                      </span>
+                                      @break
+                                  @case('served')
+                                      <span class="badge badge-success py-2 px-4" style="font-size: 16px;">
+                                          <b>Served</b>
+                                      </span>
+                                      @break
+                                  @case('end served')
+                                      <span class="badge badge-success py-2 px-4" style="font-size: 16px;">
+                                          <b>End Served</b>
+                                      </span>
+                                      @break
+                              @endswitch
                           </div>
                           <div class="col-md-4">
                               <p>Branch Name</p>
@@ -61,22 +110,22 @@
                               <b>{{ $appointment->Slot->Service->Branch->name }}</b>
                           </div>
                           <div class="col-md-4">
+                              <p>Date</p>
+                          </div>
+                          <div class="col-md-8 text-right">
+                              <b>{{ $appointment->date }}</b>
+                          </div>
+                          <div class="col-md-4">
                               <p>Service</p>
                           </div>
                           <div class="col-md-8 text-right">
                               <b>{{ $appointment->Slot->Service->name }}</b>
                           </div>
                           <div class="col-md-4">
-                              <p>Slot</p>
+                              <p>Time Slot</p>
                           </div>
                           <div class="col-md-8 text-right">
                               <b>{{ $appointment->Slot->start_time }} - {{ $appointment->Slot->end_time }}</b>
-                          </div>
-                          <div class="col-md-4">
-                              <p>Date</p>
-                          </div>
-                          <div class="col-md-8 text-right">
-                              <b>{{ $appointment->date }}</b>
                           </div>
                           <div class="col-md-12 mt-3">
                             <h3>Appointment Detail</h3>
@@ -86,7 +135,7 @@
                               <p>Queue No</p>
                           </div>
                           <div class="col-md-8 text-right">
-                              <b>{{ $appointment->number }}</b>
+                              <b class="text-primary" style="font-size: 26px">{{ $appointment->number }}</b>
                           </div>
                           <div class="col-md-4">
                               <p>Booking Code</p>
@@ -116,19 +165,17 @@
                           <div class="col-md-8 text-right">
                               <b>{{ $appointment->name }}</b>
                           </div>
-                          <div class="col-md-4">
-                              <p>Phone</p>
-                          </div>
-                          <div class="col-md-8 text-right">
-                              <b>{{ $appointment->phone }}</b>
-                          </div>
-                          <div class="col-md-4">
-                              <p>Email</p>
-                          </div>
-                          <div class="col-md-8 text-right">
-                              <b>{{ $appointment->email }}</b>
+                          <div class="col-md-12 text-center mt-4 mb-3">
+                              <a href="" class="btn btn-primary">Refresh</a>
                           </div>
                         </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <a href="https://play.google.com/store/apps/details?id=com.kyoo.android" target="_blank">
+                            <img src="{{ asset('img/playstore.png') }}" alt="" srcset="" class="img-playstore">
+                        </a>
                     </div>
                 </div>
             </div>
