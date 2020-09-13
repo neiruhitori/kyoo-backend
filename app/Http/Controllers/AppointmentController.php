5039b7c6a0facec89bbf2e309297db80e04869ea
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-09-13 16:44:32 +0700

    add validation user can not see status page after +1 day

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -15,6 +15,13 @@ class AppointmentController extends Controller
         $currently_attending = Appointment::select('number')->where('slot_id', $appointment->slot_id)->where('date', $appointment->date)->where('status', 'served')->first();
         $total_waiting = Appointment::where('slot_id', $appointment->slot_id)->where('date', $appointment->date)->where('number', '<', $appointment->number)->whereIn('status', ['book', 'check in'])->get()->count();
 
+        // user can not see the appointment status after +1 day of appointment
+        $expired_date = date('Y-m-d', strtotime('+1 day', strtotime($appointment->date)));
+        $date_now = date('Y-m-d');
+        if ($date_now > $expired_date) {
+            return redirect('https://www.kyoo.id/cloud/');
+        } 
+        
         return view('appointmentStatus', [
             'appointment' => $appointment,
             'total_waiting' => $total_waiting,
