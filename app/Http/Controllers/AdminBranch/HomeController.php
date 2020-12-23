62fed4419002aec42791f484928866c3303ee2ae
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-12-23 16:07:54 +0700

    dashboard graph for direct queue

--- app/Http/Controllers/AdminBranch/HomeController.php
+++ app/Http/Controllers/AdminBranch/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\AdminBranch\UpdateProfile;
 use App\User;
 use App\Appointment;
+use App\DirectQueue;
 use Auth;
 use DB;
 use App\Exports\AdminBranch\ReportExport;
@@ -16,18 +17,39 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $appointments = Appointment::whereHas('Slot.Service', function($query){
-            $query->where('branch_id', Auth::user()->branch_id);
-        })->whereMonth('date', date('m'))->get();
-        // $appointmentGraph = Appointment::select(DB::raw('DAY(date) as `day`'), DB::raw('count(id) as `total`'))->whereMonth('date', date('m'))->groupBy('day')->get(); // for mysql
-        $appointmentGraph = Appointment::whereHas('Slot.Service', function($query){
-            $query->where('branch_id', Auth::user()->branch_id);
-        })->select(DB::raw("date_part('day', date) as day"), DB::raw('count(id) as total'))->whereMonth('date', date('m'))->groupBy('day')->get(); // for pgsql
+        $isAppointment = Auth::user()->Branch->BranchType->is_appointment;
+        $isDirectQueue = Auth::user()->Branch->BranchType->is_direct_queue;
+
+        // Appointment Queue
+        if ($isAppointment) {
+            $appointments = Appointment::whereHas('Slot.Service', function($query){
+                $query->where('branch_id', Auth::user()->branch_id);
+            })->whereMonth('date', date('m'))->get();
+            // $appointmentGraph = Appointment::select(DB::raw('DAY(date) as `day`'), DB::raw('count(id) as `total`'))->whereMonth('date', date('m'))->groupBy('day')->get(); // for mysql
+            $appointmentGraph = Appointment::whereHas('Slot.Service', function($query){
+                $query->where('branch_id', Auth::user()->branch_id);
+            })->select(DB::raw("date_part('day', date) as day"), DB::raw('count(id) as total'))->whereMonth('date', date('m'))->groupBy('day')->get(); // for pgsql
+        }
+
+        // Direct Queue
+        if ($isDirectQueue) {
+            $directQueues = DirectQueue::whereHas('WorkstationService.Service', function($query){
+                $query->whereBranchId(Auth::user()->branch_id);
+            })->whereMonth('created_at', date('m'))->get();
+            $directQueueGraph = DirectQueue::whereHas('WorkstationService.Service', function($query){
+                $query->whereBranchId(Auth::user()->branch_id);
+            })->select(DB::raw("date_part('day', created_at) as day"), DB::raw('count(id) as total'))->whereMonth('created_at', date('m'))->groupBy('day')->get(); // for pgsql
+        }
+
         return view('adminBranch.home', [
-            'totalAppointment' => count($appointments),
-            'totalServed' => count($appointments->where('status', 'served')),
-            'totalNoShow' => count($appointments->where('status', 'no show')),
-            'appointmentGraph' => $appointmentGraph
+            'totalAppointment' => $isAppointment ? count($appointments) : 0,
+            'totalServed' => $isAppointment ? count($appointments->where('status', 'served')) : 0,
+            'totalNoShow' => $isAppointment ? count($appointments->where('status', 'no show')) : 0,
+            'appointmentGraph' => $isAppointment ? $appointmentGraph : [],
+            'totalDirectQueue' => $isDirectQueue ? count($directQueues) : 0,
+            'totalDirectQueueServed' => $isDirectQueue ? count($directQueues->where('status', 'done')) : 0,
+            'totalDirectQueueNoShow' => $isDirectQueue ? count($directQueues->where('status', 'unattend')) : 0,
+            'directQueueGraph' => $isDirectQueue ? $directQueueGraph : [],
         ]);
     }
 
