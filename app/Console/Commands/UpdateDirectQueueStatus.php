a8a96f5bf2825b57848ef7b9e1c08fd9672dd41e
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-02-19 19:06:07 +0700

    feat: new cron to update direct queue status

---
+++ app/Console/Commands/UpdateDirectQueueStatus.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\DirectQueue;
+
+class UpdateDirectQueueStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'directQueue:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to update direct queue status in case VCT forget to change';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        DirectQueue::whereStatus('waiting')->whereDate('created_at', '<', date('Y-m-d'))->update([
+            'status' => 'unattend'
+        ]);
+    }
+}
