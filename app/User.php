c45c1052da91f23d3bfec645b910191615ba3b29
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-07-25 20:29:29 +0700

    fixing email verified at

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'username', 'email', 'password', 'is_password_changed', 'role', 'token_external', 'platform', 'phone', 'branch_id'
+        'name', 'username', 'email', 'email_verified_at', 'password', 'is_password_changed', 'role', 'token_external', 'platform', 'phone', 'branch_id'
     ];
 
     /**
