1fd929c76096b897720b8e17c3d19758eb64a0c9
Muhammmad Abdis <abdissalim0@gmail.com>
2022-03-29 16:43:15 +0700

    Add debug console

--- resources/kyoo-ticket/js/pages/BookingStatus/BookingStatus.js
+++ resources/kyoo-ticket/js/pages/BookingStatus/BookingStatus.js
@@ -46,6 +46,20 @@ export default function BookingStatus() {
         service = serviceQuery.data
 
         if (service.slot) {
+            console.log(service.slot.filter(s => {
+                return s.day == getDayName(new Date(booking?.date), 'en')
+            }).sort((a, b) => {
+                if (a.start_time > b.start_time) {
+                    return 1
+                }
+                
+                if (a.start_time < b.start_time) {
+                    return -1
+                }
+
+                return 0
+            }))
+
             slot = service.slot.filter(s => {
                 return s.day == getDayName(new Date(booking?.date), 'en')
             }).sort((a, b) => {
