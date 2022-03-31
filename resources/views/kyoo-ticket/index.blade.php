574c5a33a9126cc7a843427d2a4f98ea006c13e3
Muhammmad Abdis <abdissalim0@gmail.com>
2022-03-31 11:20:04 +0700

    Add favicon

--- resources/views/kyoo-ticket/index.blade.php
+++ resources/views/kyoo-ticket/index.blade.php
@@ -11,6 +11,8 @@
 
     <title>Kyoo</title>
 
+    <link rel="icon" href="{{ asset('img/favico.png') }}" type="image/icon type">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet">
