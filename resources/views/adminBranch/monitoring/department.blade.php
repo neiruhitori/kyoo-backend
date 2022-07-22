037a0259e588c4e858fb7944ec7f41c97544f7c8
Muhammad Abdis <abdissalim0@gmail.com>
2022-07-23 00:36:42 +0700

    Redesign department monitoring

--- resources/views/adminBranch/monitoring/department.blade.php
+++ resources/views/adminBranch/monitoring/department.blade.php
@@ -3,27 +3,32 @@
 @section('content')
     <div class="row">
         <div class="col-xl-12 col-lg-7">
+            <form id="filterForm" class="mb-4">
+                <div class="form-row align-items-end">
+                    <div class="col-auto">
+                        <label for="deparmentId">Departemen</label>
+                        <select class="form-control" id="departmentId" style="width: 160px;" autocomplete="off">
+                            @foreach ($departments as $department)
+                                <option value="{{ $department->id }}">{{ $department->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary">Filter</button>
+                    </div>
+                </div>
+            </form>
+
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div>
-                            <h6 class="m-0 font-weight-bold text-primary">Monitoring Departemen</h6>
-                        </div>
-                    
-                        <div>
-                            <select class="form-control" id="departmentId" autocomplete="off">
-                                @foreach ($departments as $department)
-                                    <option value="{{ $department->id }}">{{ $department->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
+                    <h6 class="m-0 font-weight-bold text-primary">Monitoring Departemen</h6>
                 </div>
 
                 <div class="card-body">
-                    <div class="d-flex align-items-center mb-4">
+                    <div class="d-flex justify-content-between align-items-center mb-4">
                         <div class="d-flex align-items-center">
-                            <label for="refreshInterval" class="mr-2">Refresh interval (menit)</label>
+                            <label for="refreshInterval" class="mr-3 mb-0">Refresh / menit</label>
                             <select class="form-control" autocomplete="off" id="refreshInterval" style="max-width: 60px">
                                 <option value="5" selected>5</option>
                                 <option value="10">10</option>
@@ -38,13 +43,13 @@
                     </div>
 
                     <div class="table-responsive">
-                        <table class="table table-bordered mb-4" id="table">
+                        <table class="table table-bordered table-striped mb-4" id="table">
                             <thead>
                                 <tr>
                                     <th rowspan="2" class="align-middle">Nama Layanan</th>
-                                    <th rowspan="2" class="align-middle">Menunggu</th>
-                                    <th rowspan="2" class="align-middle">Dilayani</th>
-                                    <th rowspan="2" class="align-middle">Tidak Hadir</th>
+                                    <th rowspan="2" class="align-middle text-right">Menunggu</th>
+                                    <th rowspan="2" class="align-middle text-right">Dilayani</th>
+                                    <th rowspan="2" class="align-middle text-right">Tidak Hadir</th>
                                     <th colspan="3" class="text-center">Waktu Tunggu</th>
                                     <th colspan="3" class="text-center">Waktu Melayani</th>
                                 </tr>
@@ -85,8 +90,10 @@
         feedTable()
         setLastRefreshedAt()
 
-        $("#departmentId").change(function (e) {
-            departmentId = e.target.value
+        $("#filterForm").submit(function (e) {
+            e.preventDefault()
+
+            departmentId = $("#departmentId").val()
             feedTable()
         })
 
@@ -143,9 +150,9 @@
         return data.map(v => {
             return `<tr>
                 <td>${v.name}</td>
-                <td>${v.total_waiting}</td>
-                <td>${v.total_served}</td>
-                <td>${v.total_no_show}</td>
+                <td class="text-right">${v.total_waiting}</td>
+                <td class="text-right">${v.total_served}</td>
+                <td class="text-right">${v.total_no_show}</td>
                 <td class="text-center">${formatTime(v.now_waiting_duration)}</td>
                 <td class="text-center">${formatTime(v.avg_waiting_duration)}</td>
                 <td class="text-center">${formatTime(v.max_waiting_duration)}</td>
