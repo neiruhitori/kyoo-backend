79e518e7d310ce1a4da2b5eb1e5f9bacaa5b2a2a
Muhammad Abdis <abdissalim0@gmail.com>
2022-09-06 16:49:06 +0700

    Add  appointment unit test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,63 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Branch;
+use App\BranchType;
+use App\IndustryCategory;
+use App\Department;
+use App\Service;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $branch, $department, $service;
+
+    public function setupOrganization($licenseType, $queueType)
+    {
+        $this->branch = $this->createBranch($licenseType, $queueType);
+        $this->department = $this->createDepartment();
+        $this->service = $this->createService();
+    }
+
+    protected function createBranch($licenseType, $queueType)
+    {
+        $branchType = null;
+
+        if ($licenseType == 'free' && $queueType == 'appointment') {
+            $branchType = BranchType::factory()->free()->appointment();
+        }
+
+        if ($licenseType == 'free' && $queueType == 'onsite') {
+            $branchType = BranchType::factory()->free()->onsite();
+        }
+
+        if ($licenseType == 'free' && $queueType == 'exhibition') {
+            $branchType = BranchType::factory()->free()->exhibition();
+        }
+
+        $industryCategory = IndustryCategory::factory();
+
+        return Branch::factory()
+            ->for($branchType)
+            ->for($industryCategory)
+            ->create();
+    }
+
+    protected function createDepartment()
+    {
+        return Department::factory()
+            ->state(['branch_id' => $this->branch->id])
+            ->create();
+    }
+
+    protected function createService()
+    {
+        return Service::factory()
+            ->state([
+                'branch_id' => $this->branch->id,
+                'department_id' => $this->department->id
+            ])
+            ->create();
+    }
 }
---
+++ tests/Unit/AppointmentServiceTest.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Appointment;
+use App\Services\AppointmentService;
+use App\Slot;
+use Throwable;
+use App\Models\BranchScheduleTemplateDetail;
+use App\Schedule;
+use Illuminate\Support\Carbon;
+
+class AppointmentServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $appointmentService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setupOrganization('free', 'appointment');
+
+        $this->slot = Slot::factory()->state([
+            'service_id' => $this->service->id,
+            'start_time' => Carbon::now()->subHours(3)->format('H:i'),
+            'end_time' => Carbon::now()->subMinutes(1)->format('H:i')
+        ])->create();
+
+        $this->appointmentService = app(AppointmentService::class);
+    }
+
+    public function test_free_appointment_exceeded()
+    {
+        $this->slot = Slot::factory()->state(['service_id' => $this->service->id])->create();
+
+        Appointment::factory()
+            ->count(5)
+            ->state([
+                'branch_id' => $this->branch->id,
+                'service_id' => $this->service->id,
+                'date' => date('Y-m-d'),
+                'slot_id' => $this->slot->id
+            ])
+            ->create();
+
+        $message = 'Appointment gratis tersedia';
+
+        try {
+            $this->appointmentService->create([
+                'branch_id' => $this->branch->id,
+                'date' => date('Y-m-d')
+            ]);
+        } catch (Throwable $e) {
+            $message = $e->getMessage();
+        }
+
+        $this->assertEquals($message, 'Batas appointment gratis hari ini terlampaui');
+    }
+
+    public function test_duplicate_appointments()
+    {
+        $email = 'abdis@mail.com';
+        $phone = '0851569776458';
+
+        Appointment::factory()->create([
+            'email' => $email,
+            'phone' => $phone,
+            'branch_id' => $this->branch->id,
+            'service_id' => $this->service->id,
+            'date' => date('Y-m-d'),
+            'slot_id' => $this->slot->id
+        ]);
+
+        $message = 'Appointment belum terdaftar';
+
+        try {
+            $this->appointmentService->create([
+                'branch_id' => $this->branch->id,
+                'date' => date('Y-m-d'),
+                'email' => $email,
+                'phone' => $phone,      
+                'slot_id' => $this->slot->id
+            ]);
+        } catch(Throwable $e) {
+            $message = $e->getMessage();
+        }
+
+        $this->assertEquals($message, 'Appointment telah terdaftar');
+    }
+
+    public function test_appointment_on_holiday()
+    {
+        $holiday = BranchScheduleTemplateDetail::factory()
+            ->state(['branch_id' => $this->branch->id])
+            ->create();
+
+        $message = 'Sesi appointment tersedia';
+
+        try {
+            $this->appointmentService->create([
+                'branch_id' => $this->branch->id,
+                'date' => $holiday->date,
+                'slot_id' => $this->slot->id
+            ]);
+        } catch(Throwable $e) {
+            $message = $e->getMessage();
+        }
+
+        $this->assertEquals($message, 'Sesi appointment tidak tersedia');
+    }
+
+    public function test_appointment_on_closed_day()
+    {
+        $closedSchedule = Schedule::factory()
+            ->state([
+                'branch_id' => $this->branch->id,
+                'status' => 'closed'
+            ])
+            ->create();
+
+        $date = date('Y-m-d', strtotime($closedSchedule->day . ' september 2022'));
+
+        $message = 'Sesi appointment tersedia';
+
+        try {
+            $this->appointmentService->create([
+                'branch_id' => $this->branch->id,
+                'date' => $date,
+                'slot_id' => $this->slot->id
+            ]);
+        } catch(Throwable $e) {
+            $message = $e->getMessage();
+        }
+
+        $this->assertEquals($message, 'Sesi appointment tidak tersedia');
+    }
+
+    public function test_past_schedule_appointment()
+    {
+        $message = 'Sesi appointment tersedia';
+
+        try {
+            $this->appointmentService->create([
+                'branch_id' => $this->branch->id,
+                'date' => date('Y-m-d'),
+                'slot_id' => $this->slot->id
+            ]);
+        } catch(Throwable $e) {
+            $message = $e->getMessage();
+        }
+
+        $this->assertEquals($message, 'Sesi appointment sudah berakhir');
+    }
+
+    public function test_store_appointment()
+    {
+        $slot = Slot::factory()
+            ->state([
+                'service_id' => $this->service->id,
+                'start_time' => date('H:i'),
+                'end_time' =>  date('H:i', strtotime('+3 hours'))
+            ])
+            ->create();
+
+        $this->appointmentService->create([
+            'branch_id' => $this->branch->id,
+            'date' => date('Y-m-d'),
+            'slot_id' => $slot->id,
+            'name' => 'Abdis',
+            'email' => 'abdis@mail.com',
+            'phone' => '085156976458'
+        ]);
+
+        $this->assertDatabaseCount('appointments', 1);
+    }
+}
