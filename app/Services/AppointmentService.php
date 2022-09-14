53a3026e365311a64c0619e51e8cb581b60474d0
Muhammad Abdis <abdissalim0@gmail.com>
2022-09-15 00:49:22 +0700

    Fix appointment race condition

--- app/Services/AppointmentService.php
+++ app/Services/AppointmentService.php
@@ -8,116 +8,126 @@ use App\Appointment;
 use App\Models\BranchScheduleTemplateDetail;
 use App\Schedule;
 use App\Events\AppointmentCreated;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
 
 class AppointmentService
 {
     public function create($data): ?Appointment
     {
-        $date = date('Y-m-d', strtotime($data['date']));
+        try {
+            $date = date('Y-m-d', strtotime($data['date']));
 
-        $branch = Branch::find($data['branch_id']);
-        $slot = Slot::find($data['slot_id']);
+            $branch = Branch::find($data['branch_id']);
+            $slot = Slot::find($data['slot_id']);
 
-        // Limit free appointments
-        $totalTodayAppointments = Appointment::where([
-            'branch_id' => $data['branch_id'],
-            'date' => $date
-        ])->count();
+            // Limit free appointments
+            $totalTodayAppointments = Appointment::where([
+                'branch_id' => $data['branch_id'],
+                'date' => $date
+            ])->count();
 
-        if (
-            !$branch->BranchType->is_premium &&
-            $totalTodayAppointments >= config('appointment.free_appointment_limit')
-        ) {
-            throw new \Exception('Batas appointment gratis hari ini terlampaui');
-        }
+            if (
+                !$branch->BranchType->is_premium &&
+                $totalTodayAppointments >= config('appointment.free_appointment_limit')
+            ) {
+                throw new \Exception('Batas appointment gratis hari ini terlampaui');
+            }
 
-        // Prevent double appointments
-        $email = $phone = '';
+            // Prevent double appointments
+            $email = $phone = '';
 
-        if (isset($data['email'])) $email = $data['email'];
-        if (isset($data['phone'])) $phone = $data['phone'];
+            if (isset($data['email'])) $email = $data['email'];
+            if (isset($data['phone'])) $phone = $data['phone'];
 
-        $sameAppointment = Appointment::where([
-                'slot_id' => $data['slot_id'],
+            $sameAppointment = Appointment::where([
+                    'slot_id' => $data['slot_id'],
+                    'date' => $date
+                ])
+                ->where(function ($query) use ($email, $phone) {
+                    $query->where('email', $email)
+                        ->orWhere('phone', $phone);
+                })
+                ->sharedLock()
+                ->first();
+
+            if ($sameAppointment) {
+                throw new \Exception('Appointment telah terdaftar');
+            }
+
+            // Prevent appointment on empty slots
+            $todayAppointmentsBySlot = Appointment::where([
+                    'slot_id' => $data['slot_id'],
+                    'date' => $date
+                ])
+                ->sharedLock()
+                ->get();
+            
+            if (count($todayAppointmentsBySlot) >= $slot->max_slots) {
+                throw new \Exception('Sesi appointment tidak tersedia');
+            }
+
+            // Prevent appointment on holidays
+            $selectedHoliday = BranchScheduleTemplateDetail::where([
+                'branch_id' => $data['branch_id'],
+                'date' => $date
+            ])->first();
+
+            if ($selectedHoliday) {
+                throw new \Exception('Sesi appointment tidak tersedia');
+            }
+
+            // Prevent appointment on closed days
+            $day = strtolower(date('l', strtotime($data['date'])));
+
+            $selectedSchedule = Schedule::where([
+                'branch_id' => $data['branch_id'],
+                'day' => $day
+            ])->first();
+
+            if ($selectedSchedule && $selectedSchedule->status == 'closed') {
+                throw new \Exception('Sesi appointment tidak tersedia');
+            }
+
+            // Can't select past
+            $appointmentEndDateTime = date('Y-m-d H:i:s', strtotime($data['date'] . ' ' . $slot->end_time . ':00'));
+            $currentDateTime = date('Y-m-d H:i:s');
+
+            if ($appointmentEndDateTime < $currentDateTime) {
+                throw new \Exception('Sesi appointment sudah berakhir');
+            }
+
+            $lastAppointment = Appointment::where([
+                'branch_id' => $data['branch_id'],
                 'date' => $date
             ])
-            ->where(function ($query) use ($email, $phone) {
-                $query->where('email', $email)
-                    ->orWhere('phone', $phone);
-            })
-            ->first();
+                ->orderByDesc('number')
+                ->sharedLock()
+                ->first();
 
-        if ($sameAppointment) {
-            throw new \Exception('Appointment telah terdaftar');
-        }
-
-        // Prevent appointment on empty slots
-        $totalTodayAppointmentsBySlot = Appointment::where([
-                'slot_id' => $data['slot_id'],
-                'date' => $date
-            ])
-            ->count();
+            $data['booking_code'] = $this->generateBookingCode();
+            $data['number'] = $lastAppointment ? (int) $lastAppointment->number + 1 : 1;
+            
+            // Store appointment to db
+            $appointment = Appointment::create($data);
         
-        if ($totalTodayAppointmentsBySlot >= $slot->max_slots) {
-            throw new \Exception('Sesi appointment tidak tersedia');
+            // Dispatch created event
+            AppointmentCreated::dispatch($appointment);
+
+            return $appointment;
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            throw $e;
         }
-
-        // Prevent appointment on holidays
-        $selectedHoliday = BranchScheduleTemplateDetail::where([
-            'branch_id' => $data['branch_id'],
-            'date' => $date
-        ])->first();
-
-        if ($selectedHoliday) {
-            throw new \Exception('Sesi appointment tidak tersedia');
-        }
-
-        // Prevent appointment on closed days
-        $day = strtolower(date('l', strtotime($data['date'])));
-
-        $selectedSchedule = Schedule::where([
-            'branch_id' => $data['branch_id'],
-            'day' => $day
-        ])->first();
-
-        if ($selectedSchedule && $selectedSchedule->status == 'closed') {
-            throw new \Exception('Sesi appointment tidak tersedia');
-        }
-
-        // Can't select past
-        $appointmentEndDateTime = date('Y-m-d H:i:s', strtotime($data['date'] . ' ' . $slot->end_time . ':00'));
-        $currentDateTime = date('Y-m-d H:i:s');
-
-        if ($appointmentEndDateTime < $currentDateTime) {
-            throw new \Exception('Sesi appointment sudah berakhir');
-        }
-
-        $totalLastAppointments = Appointment::where([
-            'branch_id' => $data['branch_id'],
-            'date' => $date
-        ])->count();
-
-        $data['booking_code'] = $this->generateBookingCode();
-        $data['number'] = $totalLastAppointments + 1;
-        
-        // Store appointment to db
-        $appointment = Appointment::create($data);
-    
-        // Dispatch created event
-        AppointmentCreated::dispatch($appointment);
-
-        return $appointment;
     }
 
     private function generateBookingCode()
     {
         $permittedChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-        $charsLen = strlen($permittedChars);
         $randomString = '';
         for($i = 0; $i < 5; $i++) {
-            $randomString .= $permittedChars[mt_rand(0, $charsLen - 1)];
+            $randomString .= $permittedChars[mt_rand(0, strlen($permittedChars) - 1)];
         }
     
         return $randomString;
