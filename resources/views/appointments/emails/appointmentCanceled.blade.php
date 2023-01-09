0d912cfe5d1daf5ff7a6503ef36c0f7552283aa3
Muhammad Abdis <abdissalim0@gmail.com>
2023-01-09 15:31:00 +0700

    fix: canceled appointment email

--- resources/views/appointments/emails/appointmentCanceled.blade.php
+++ resources/views/appointments/emails/appointmentCanceled.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Appointment Dibatalkan
 
-Appointment Anda pada tanggal {{ $appointment->date->format('d-m-Y') }} di {{ $appointment->Branch->name }} dibatalkan. Klik link dibawah untuk melihat status antrian Anda.
+Appointment Anda pada tanggal {{ date('Y-m-d', strtotime($appointment->date)) }} di {{ $appointment->Branch->name }} dibatalkan. Klik link dibawah untuk melihat status antrian Anda.
 
 @component('mail::button', ['url' => $appointmentStatusURL])
 Cek Appointment Saya
