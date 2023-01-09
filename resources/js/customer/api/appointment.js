95e0176261da52f2ab7c736c25af354a67968a78
Muhammad Abdis <abdissalim0@gmail.com>
2023-01-09 16:17:33 +0700

    fix: catch appointment error on web customer

--- resources/js/customer/api/appointment.js
+++ resources/js/customer/api/appointment.js
@@ -3,14 +3,32 @@ import http from '../utils/http'
 export function createAppointment(data) {
     return http.post('appointment', data)
         .then(res => res.data)
+        .catch(err => {
+            return {
+                success: false,
+                message: err.response?.data?.message
+            }
+        })
 }
 
 export function getAppointmentById(id) {
     return http.get(`appointment/${id}`)
         .then(res => res.data)
+        .catch(err => {
+            return {
+                success: false,
+                message: err.response?.data?.message
+            }
+        })
 }
 
 export function cancelAppointment(id) {
     return http.patch(`appointment/${id}/cancel`)
         .then(res => res.data)
+        .catch(err => {
+            return {
+                success: false,
+                message: err.response?.data?.message
+            }
+        })
 }
