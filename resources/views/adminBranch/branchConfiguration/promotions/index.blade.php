1fedbd0c6e25f942bac9f72b146782c826e5d4ce
Muhammad Abdis <abdissalim0@gmail.com>
2023-01-19 22:08:20 +0700

    feat: add empty state to promotion

--- resources/views/adminBranch/branchConfiguration/promotions/index.blade.php
+++ resources/views/adminBranch/branchConfiguration/promotions/index.blade.php
@@ -4,8 +4,8 @@
   <div style="max-width: 800px; margin: 0 auto;">
     <h3 class="mb-3">Konten Promosi</h3>
 
-    <div class="mb-4">
-      <h6>Tambah Promosi</h6>
+    <div class="mb-5">
+      <h6 class="font-weight-bold">Tambah Promosi</h6>
 
       <div>
         <a href="{{ route('admin-branch.branch-configuration.promotions.image.create') }}" class="btn btn-secondary mr-1">
@@ -21,25 +21,40 @@
     </div>
 
     <div>
-      <h6>Daftar Promosi</h6>
+      <h6 class="font-weight-bold">Daftar Promosi</h6>
 
       <div class="row">
-        <div class="col-md-3" >
+        {{-- <div class="col-md-3" >
           <div class="rounded" style="height: 360px; background-color: #DDD"></div>
-        </div>
+        </div> --}}
 
-        <div class="col-md-3">
-          <div class="rounded" style="height: 360px; background-color: #DDD"></div>
-        </div>
+        <div class="col-md-12 mt-4">
+          <div class="circle mb-3">
+            <span class="fas fa-box-open" style="color: #AAA"></span>
+          </div>
 
-        <div class="col-md-3">
-          <div class="rounded" style="height: 360px; background-color: #DDD"></div>
-        </div>
-
-        <div class="col-md-3">
-          <div class="rounded" style="height: 360px; background-color: #DDD"></div>
+          <div style="max-width: 360px; margin: 0 auto;">
+            <h4 class="text-center">Tidak ada konten</h4>
+            <p class="text-center">Pilih tombol diatas untuk menambahkan promosi</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 @endsection
+
+@push('css')
+<style>
+  .circle {
+    border-radius: 100%;
+    background-color: #DDD;
+    max-width: 100px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    font-size: 2.25rem;
+    margin: 0 auto;
+  }
+</style>
+@endpush
