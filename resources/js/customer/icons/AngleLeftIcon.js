9289af4738ea891c506f3fe22fa05382375bbc50
NalendraUtama <nalendrautama@gmail.com>
2025-05-02 11:37:33 +0700

    Feat: component angle left icon

---
+++ resources/js/customer/icons/AngleLeftIcon.js
@@ -0,0 +1,9 @@
+import SvgIcon from '../components/SvgIcon'
+
+function AngleLeftIcon(props) {
+    return <SvgIcon viewBox="0 0 640 512" {...props}>
+       <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
+    </SvgIcon>
+}
+
+export default AngleLeftIcon
